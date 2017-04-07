b5fc72d00b9714d3df421c2f696c77eef5011904
mpkerry <mkerry@g.harvard.edu>
2017-04-07 08:32:58 -0400

    tests! spring sec config

--- grails-app/controllers/cscie56/demo/ShoppingCartController.groovy
+++ grails-app/controllers/cscie56/demo/ShoppingCartController.groovy
@@ -1,11 +1,17 @@
 package cscie56.demo
 
+import grails.plugin.springsecurity.annotation.Secured
+
 class ShoppingCartController {
 
     ShoppingCartService shoppingCartService
 
-    def index() { }
+    @Secured([Role.ROLE_USER,Role.ROLE_ADMIN,Role.ROLE_ANONYMOUS,"ROLE_TESTER"])
+    def index() {
+        render "It works!"
+    }
 
+    @Secured([Role.ROLE_USER,Role.ROLE_ADMIN])
     def addToCart(ShoppingCart cart, item, Integer quantity) {
         if (cart) {
             shoppingCartService.addItemToCart(cart,item,quantity)
